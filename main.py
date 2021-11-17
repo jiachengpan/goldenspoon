6a36682c06c1c9de9b93da1f9de7b21fcadb6d07
Jiacheng Pan <jiacheng.pan@enflame-tech.com>
2021-11-17 17:02:52 +0800

    export labels

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import goldenspoon
 import numpy as np
-
 import argparse
+import datetime
+from dateutil import parser
+from dateutil.relativedelta import relativedelta
 
-parser = argparse.ArgumentParser(description='Run!')
-parser.add_argument('end_date', type=str, help='')
+argparser = argparse.ArgumentParser(description='Run!')
+argparser.add_argument('end_date', type=str, help='')
 
-args = parser.parse_args()
+args = argparser.parse_args()
 
-db  = goldenspoon.Database('data')
-ind = goldenspoon.Indicator(db, end_date=args.end_date)
+end_date = parser.parse(args.end_date)
 
+db = goldenspoon.Database('data')
+
+columns = [
+        goldenspoon.Indicator.k_column_date,
+        #goldenspoon.Indicator.k_stock_column_code,
+        goldenspoon.Indicator.k_stock_column_close_price,
+        ]
+for i in range(3):
+    new_end_date = end_date + relativedelta(months=i+1)
+
+    print(i, end_date, new_end_date)
+
+    ind = goldenspoon.Indicator(db, end_date=new_end_date.date().isoformat())
+    labels = ind.get_stocks_timed().dropna(subset=columns) \
+            .sort_values(goldenspoon.Indicator.k_column_date) \
+            .groupby(goldenspoon.Indicator.k_stock_column_code)[columns] \
+            .last()
+    labels.to_pickle(f'labels.{i+1}_month.{args.end_date}.pickle')
+
+ind = goldenspoon.Indicator(db, end_date=end_date.date().isoformat())
 database = goldenspoon.compute_indicators(ind)
 print(database.describe())
 database.to_pickle(f'indicators.{args.end_date}.pickle')
