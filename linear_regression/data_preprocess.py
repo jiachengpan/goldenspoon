f7d00d6728c159be152f72c97e7afdb49b8d6084
xiaoying.zhang <xiaoying.zhang@enflame-tech.com>
2022-01-05 06:18:06 +0000

    refactor the code

--- linear_regression/data_preprocess.py
+++ linear_regression/data_preprocess.py
@@ -1,68 +1,81 @@
 import pandas as pd
 
+
 class DataPreprocess():
-    def __init__(self,data_path, train_date_list,test_date_list, n_month_predict, i_month_label, indicator_list):
+    def __init__(self, data_path, train_date_list, test_date_list, n_month_predict, i_month_label, indicator_list):
         self.data_path = data_path
         self.train_date_list = train_date_list
         self.test_date_list = test_date_list
         self.n_month_predict = n_month_predict
-        self.i_month_label = i_month_label  # '1_predict_changerate_price' or '1_predict_absvalue_price'
+        # '1_predict_changerate_price' or '1_predict_absvalue_price'
+        self.i_month_label = i_month_label
         self.indicator_list = indicator_list
 
-    ## 1. Data generation
+    # 1. Data generation
     def merge_df_indicator_label(self, df_indicator, df_label_list):
         df_indicator_label = df_indicator
         for m in range(len(df_label_list)):
             df_label = df_label_list[m]
             df_label = df_label.reset_index()
-            if m==0:
+            if m == 0:
                 df_label.columns = ['id', 'date', '0_label']
                 df_label = df_label.drop(columns=['date'], axis=1)
             else:
                 if '_predict_changerate_price' in self.i_month_label:
-                    df_label.columns = ['id', 'date', "predict_absvalue_price", str(m)+'_predict_changerate_price']
+                    df_label.columns = ['id', 'date', "predict_absvalue_price", str(
+                        m)+'_predict_changerate_price']
                     df_label = df_label.drop(columns=['date'], axis=1)
-                    df_label = df_label.drop(columns=['predict_absvalue_price'], axis=1)
+                    df_label = df_label.drop(
+                        columns=['predict_absvalue_price'], axis=1)
                 elif '_predict_absvalue_price' in self.i_month_label:
-                    df_label.columns = ['id', 'date', str(m)+'_predict_absvalue_price', "predict_changerate_price"]
+                    df_label.columns = ['id', 'date', str(
+                        m)+'_predict_absvalue_price', "predict_changerate_price"]
                     df_label = df_label.drop(columns=['date'], axis=1)
-                    df_label = df_label.drop(columns=['predict_changerate_price'], axis=1)
-            df_indicator_label = df_indicator_label.merge(df_label, on=['id'], how='inner')
+                    df_label = df_label.drop(
+                        columns=['predict_changerate_price'], axis=1)
+            df_indicator_label = df_indicator_label.merge(
+                df_label, on=['id'], how='inner')
         return df_indicator_label
 
     def merge_indicator_label(self, df_indicator, label_pickle_list):
-        df_label_list= []
+        df_label_list = []
         for filename in label_pickle_list:
             df_label = pd.read_pickle(filename)
             df_label_list.append(df_label)
 
-        df_indicator_label = self.merge_df_indicator_label(df_indicator, df_label_list)
+        df_indicator_label = self.merge_df_indicator_label(
+            df_indicator, df_label_list)
         return df_indicator_label
 
     def get_train_indicator_label(self, df_indicator_label):
         X_df = df_indicator_label[self.indicator_list]
-        self.end_date_stock_price = df_indicator_label.iloc[:,df_indicator_label.columns.str.endswith('0_label')]
-        Y_df = df_indicator_label.iloc[:,df_indicator_label.columns.str.endswith(self.i_month_label)]
+        self.end_date_stock_price = df_indicator_label.iloc[:,
+                                                            df_indicator_label.columns.str.endswith('0_label')]
+        Y_df = df_indicator_label.iloc[:, df_indicator_label.columns.str.endswith(
+            self.i_month_label)]
         Y_df = pd.concat([self.end_date_stock_price, Y_df], axis=1)
-        ID_df = df_indicator_label.iloc[:,df_indicator_label.columns.str.endswith('id')]
+        ID_df = df_indicator_label.iloc[:,
+                                        df_indicator_label.columns.str.endswith('id')]
         return X_df, Y_df, ID_df
 
     def run(self):
-        ### 
         totaldate_X_df = pd.DataFrame()
         totaldate_Y_df = pd.DataFrame()
         for date in self.train_date_list:
-            print("train date:",date)
+            print("train date:", date)
             indicator_pickle = self.data_path + 'indicators.' + date + '.pickle'
             df_indicator = pd.read_pickle(indicator_pickle)
             self.all_indicator_list = list(df_indicator.columns.values)[1:]
             if not df_indicator.empty:
                 label_pickle_list = []
-                for m in range(self.n_month_predict+1): ## include 0_month
-                    label_pickle = self.data_path + 'labels.' + str(m) + '_month.'+ date +'.pickle'
+                for m in range(self.n_month_predict+1):  # include 0_month
+                    label_pickle = self.data_path + 'labels.' + \
+                        str(m) + '_month.' + date + '.pickle'
                     label_pickle_list.append(label_pickle)
-                df_indicator_label = self.merge_indicator_label(df_indicator, label_pickle_list)
-                X_df, Y_df, train_ID_df = self.get_train_indicator_label(df_indicator_label)
+                df_indicator_label = self.merge_indicator_label(
+                    df_indicator, label_pickle_list)
+                X_df, Y_df, train_ID_df = self.get_train_indicator_label(
+                    df_indicator_label)
                 totaldate_X_df = pd.concat([totaldate_X_df, X_df], axis=0)
                 totaldate_Y_df = pd.concat([totaldate_Y_df, Y_df], axis=0)
             else:
@@ -70,19 +83,21 @@ class DataPreprocess():
         x_train, y_train = totaldate_X_df, totaldate_Y_df
         # x_train, x_test, y_train, y_test = train_test_split(totaldate_X_df, totaldate_Y_df, random_state=1)
 
-        ###
         for date in self.test_date_list:
-            print("test date:",date)
+            print("test date:", date)
             indicator_pickle = self.data_path + 'indicators.' + date + '.pickle'
             df_indicator = pd.read_pickle(indicator_pickle)
             self.all_indicator_list = list(df_indicator.columns.values)[1:]
             if not df_indicator.empty:
                 label_pickle_list = []
-                for m in range(self.n_month_predict+1): ## include 0_month
-                    label_pickle = self.data_path + 'labels.' + str(m) + '_month.'+ date +'.pickle'
+                for m in range(self.n_month_predict+1):  # include 0_month
+                    label_pickle = self.data_path + 'labels.' + \
+                        str(m) + '_month.' + date + '.pickle'
                     label_pickle_list.append(label_pickle)
-                df_indicator_label = self.merge_indicator_label(df_indicator, label_pickle_list)
-                X_df, Y_df, test_ID_df = self.get_train_indicator_label(df_indicator_label)
+                df_indicator_label = self.merge_indicator_label(
+                    df_indicator, label_pickle_list)
+                X_df, Y_df, test_ID_df = self.get_train_indicator_label(
+                    df_indicator_label)
             else:
                 print("{} is a empty dataframe!".format(indicator_pickle))
         x_test, y_test = X_df, Y_df
--- linear_regression/regress.py
+++ linear_regression/regress.py
@@ -6,11 +6,12 @@ import os
 from csv import writer
 from data_preprocess import DataPreprocess
 import argparse
-from result_analysis import drop_small_change_stock,perf_measure,perf_measure_per_stock
+from result_analysis import drop_small_change_stock, perf_measure, perf_measure_per_stock
+
 
 def get_args():
     parser = argparse.ArgumentParser(description='Goldenspoon Regression',
-                        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--data_path",
                         default='regress_data/past_quater_4/',
                         help="Provide regression data path.")
@@ -21,7 +22,6 @@ def get_args():
         "--indicator_use_list",
         nargs="+",
         default=[])
-
     parser.add_argument(
         "--train_date_list",
         nargs="+",
@@ -30,59 +30,64 @@ def get_args():
         "--test_date_list",
         nargs="+",
         default=['2021-09-30'])
-
     parser.add_argument(
         "--n_month_predict",
         type=int,
         default=3,
         help="N month to predict.")
-    parser.add_argument("--predict_mode", default="predict_changerate_price", choices=['predict_changerate_price','predict_absvalue_price'])
-
-
+    parser.add_argument("--predict_mode", default="predict_changerate_price",
+                        choices=['predict_changerate_price', 'predict_absvalue_price'])
     return parser.parse_args()
+
 
 class Regress():
     def __init__(self, regress_model, save_path_permonth, i_month_label, indicator_list):
         self.model = regress_model
         self.save_path_permonth = save_path_permonth
-        self.i_month_label = i_month_label # '1_predict_changerate_price' or '1_predict_absvalue_price'
+        # '1_predict_changerate_price' or '1_predict_absvalue_price'
+        self.i_month_label = i_month_label
         self.indicator_list = indicator_list
 
-    ## Linear regression
-    def run(self,data_path,train_date_list,test_date_list,n_month_predict):
-        ## 数据预处理
-        x_train, x_test, y_train, y_test, test_ID_df = DataPreprocess(data_path, train_date_list,test_date_list,n_month_predict,self.i_month_label,self.indicator_list).run()
+    # Linear regression
+    def run(self, data_path, train_date_list, test_date_list, n_month_predict):
+        # 数据预处理
+        x_train, x_test, y_train, y_test, test_ID_df = DataPreprocess(
+            data_path, train_date_list, test_date_list, n_month_predict, self.i_month_label, self.indicator_list).run()
         print("-----x_train.shape:{}\n".format(x_train.shape))
         print("-----x_test.shape:{}\n".format(x_test.shape))
         print("-----y_train.shape:{}\n".format(y_train.shape))
         print("-----y_test.shape:{}\n".format(y_test.shape))
 
-        ## 获取训练集的label、测试集的label、测试集的label对应的股票ID
+        # 获取训练集的label、测试集的label、测试集的label对应的股票ID
         cur_stock_price_test = y_test['0_label']
-        y_train = y_train[self.i_month_label] 
+        y_train = y_train[self.i_month_label]
         y_test = y_test[self.i_month_label]
         y_testID = test_ID_df['id']
 
-        ## 模型训练
+        # 模型训练
         self.model.fit(x_train, y_train)
         y_pred = self.model.predict(x_test)
         # R2_y_pred_y_test = r2_score(y_test, y_pred)
-        indicator_weight = list(zip(self.indicator_list, list(self.model.coef_)))
+        indicator_weight = list(
+            zip(self.indicator_list, list(self.model.coef_)))
 
-        ## 打印回归模型权重参数
-        with open(self.save_path_permonth + 'indicator_weight.log','w') as f:
+        # 打印回归模型权重参数
+        with open(self.save_path_permonth + 'indicator_weight.log', 'w') as f:
             for i_weight in indicator_weight:
                 f.write(str(i_weight))
-                f.write('\n' )
+                f.write('\n')
             f.close()
 
-        ## 结果分析
-        valid_stock_list, y_pred_valid, y_true_valid, y_stock_id_valid = drop_small_change_stock(y_pred=y_pred, y_true=y_test, drop_ponit=0.15, y_stock_id=y_testID)
+        # 结果分析
+        valid_stock_list, y_pred_valid, y_true_valid, y_stock_id_valid = drop_small_change_stock(
+            y_pred=y_pred, y_true=y_test, drop_ponit=0.15, y_stock_id=y_testID)
         full_stock_list = y_test.shape[0]
         # TP, FP, TN, FN = perf_measure(y_pred, y_test, cur_stock_price_test)
-        TP, FP, TN, FN = perf_measure(y_pred_valid, y_true_valid, cur_stock_price_test, self.i_month_label, self.save_path_permonth)
-        stock_pred_correctness = perf_measure_per_stock(full_stock_list, valid_stock_list, y_pred_valid, y_true_valid, y_stock_id_valid)
-        with open(self.save_path_permonth + 'stock_pred_correctness.csv', 'a', newline='') as f_object: 
+        TP, FP, TN, FN = perf_measure(
+            y_pred_valid, y_true_valid, cur_stock_price_test, self.i_month_label, self.save_path_permonth)
+        stock_pred_correctness = perf_measure_per_stock(
+            full_stock_list, valid_stock_list, y_pred_valid, y_true_valid, y_stock_id_valid)
+        with open(self.save_path_permonth + 'stock_pred_correctness.csv', 'a', newline='') as f_object:
             writer_object = writer(f_object)
             writer_object.writerow(stock_pred_correctness)
             f_object.close()
@@ -90,8 +95,9 @@ class Regress():
         # self.draw(y_pred, y_test, cur_stock_price_test, R2_y_pred_y_test, self.i_month_label, self.save_path_permonth, show_interval=50)
         return
 
+
 if __name__ == '__main__':
-    ## base parameter
+    # base parameter
     args = get_args()
     data_path = args.data_path
     save_path = args.save_path
@@ -99,14 +105,17 @@ if __name__ == '__main__':
     indicator_use_list = args.indicator_use_list
     train_date_list = args.train_date_list
     test_date_list = args.test_date_list
-    predict_mode = args.predict_mode #'predict_changerate_price' or 'predict_absvalue_price'
+    # 'predict_changerate_price' or 'predict_absvalue_price'
+    predict_mode = args.predict_mode
     regress_model = linear_model.LinearRegression()
 
     # 如果没有指定使用的indicator列表，则使用全部的indicator训练
     if indicator_use_list == []:
-        indicator_pickle = args.data_path + 'indicators.' + train_date_list[0] + '.pickle'
+        indicator_pickle = args.data_path + \
+            'indicators.' + train_date_list[0] + '.pickle'
         df_indicator = pd.read_pickle(indicator_pickle)
-        all_indicator_list = list(df_indicator.columns.values)[1:] # exclude 'id'
+        all_indicator_list = list(df_indicator.columns.values)[
+            1:]  # exclude 'id'
         indicator_list = all_indicator_list
         print("-----indicator_list:{}\n".format(indicator_list))
     else:
@@ -115,12 +124,13 @@ if __name__ == '__main__':
     for i in range(n_month_predict):
         i_month_predict = i+1
         print("-----i_month_predict:{}\n".format(i_month_predict))
-        i_month_label = str(i_month_predict) +'_'+ predict_mode
+        i_month_label = str(i_month_predict) + '_' + predict_mode
         print("-----i_month_label:{}\n".format(i_month_label))
 
-        save_path_permonth = save_path +'/' + str(i_month_predict) +'_month/'
+        save_path_permonth = save_path + '/' + str(i_month_predict) + '_month/'
         if not os.path.exists(save_path_permonth):
             os.makedirs(save_path_permonth)
 
-        r = Regress(regress_model, save_path_permonth, i_month_label, indicator_list)
+        r = Regress(regress_model, save_path_permonth,
+                    i_month_label, indicator_list)
         r.run(data_path, train_date_list, test_date_list, n_month_predict)
