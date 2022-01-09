4129453ba0f96f938a593df64c21a958b81c3c5c
Jiacheng Pan <jiacheng.pan@enflame-tech.com>
2022-01-09 17:57:21 +0800

    add notebook for EDA / exploratory data analysis also mild updates to data generation and requirements

--- generate_data/run.sh
+++ generate_data/run.sh
@@ -3,10 +3,6 @@
 set -e
 set -x
 
-# dates="
-#        2020-09-30
-#        "
-
 dates="
        2020-09-30
        2020-12-31
@@ -15,15 +11,13 @@ dates="
        2021-09-30
        "
 
-# dates="
-#        2021-06-30
-#        2021-09-30
-#        "
+[[ -z $past_quarters ]] && past_quarters=4
+[[ -z $output_dir ]]    && output_dir="../linear_regression/regress_data/past_quater_${past_quater_number}/"
 
-past_quater_number='4'
-regress_dir='../linear_regression/regress_data/past_quater_'${past_quater_number}'/'
+mkdir -p $output_dir
+
 for end_date in $dates; do
-  python main.py $end_date ${regress_dir} ${past_quater_number}&
+  python main.py ${end_date} ${output_dir} ${past_quarters} &
 done
 
 wait
