58f77ac4896a9c091ee0d424116426c2c7a74925
Jiacheng Pan <jiacheng.pan.sh@gmail.com>
2022-01-20 10:50:24 +0800

    add 2021 Q4 data (complete up to 1231)

--- generate_data/run.sh
+++ generate_data/run.sh
@@ -9,10 +9,11 @@ dates="
        2021-03-31
        2021-06-30
        2021-09-30
+       2021-12-31
        "
 
 [[ -z $past_quarters ]] && past_quarters=4
-[[ -z $output_dir ]]    && output_dir="../linear_regression/regress_data/past_quater_${past_quater_number}/"
+[[ -z $output_dir ]]    && output_dir="../linear_regression/regress_data/past_quater_${past_quarters}/"
 
 mkdir -p $output_dir
 
